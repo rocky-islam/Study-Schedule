04a1790222ea440dd3b8fd8f597fef8716a9cae4
rocky-islam <62227570+rocky-islam@users.noreply.github.com>
2022-09-29 18:46:36 +0600

    question done

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -4,7 +4,8 @@ import './Question.css'
 const Question = () => {
     return (
       <div>
-        <div>
+        <div className="question">
+          <br />
           <h3>Q.How does react work</h3>
           <p>
             Answer: ReactJS divides the UI into isolated reusable pieces of code
@@ -13,7 +14,8 @@ const Question = () => {
             props. It's possible to have as many components as necessary without
             cluttering your code
           </p>
-
+          <br />
+          <br />
           <h3>Q.props vs state in react</h3>
           <p>
             Answer: Props are used to pass data from one component to another.
@@ -21,6 +23,17 @@ const Question = () => {
             only and cannot be passed to other components. The this. setState
             property is used to update the state values in the component.
           </p>
+          <br />
+          <br />
+          <h3>Q.when should you use useeffect</h3>
+          <ol>
+            <li>useEffect() is for side-effects</li>
+            <li>Dependencies argument</li>
+            <li>Component lifecycle</li>
+            <li>Component did mount / Component did update</li>
+            <li>Side-effect cleanup</li>
+            <li>Fetching data</li>
+          </ol>
         </div>
       </div>
     );
